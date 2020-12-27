062e0bde481a6f5d39943d834b7b8ee3472e31b0
tigertang <tigertang.zju@gmail.com>
2020-12-27 21:11:39 +0800

    Update deploy.py

--- .vscode/deploy.py
+++ .vscode/deploy.py
@@ -3,27 +3,32 @@ import sys
 import subprocess
 from typing import Optional
 
+
+ADB = "adb"
 ADB_ADDR = "[todo] adb serial number"
+GDB_SERVER = "/data/local/tmp/gdbserver"
 
 
 def copy(local_bin_path: str, remote_bin_name: Optional[str] = None):
     if remote_bin_name is None:
         remote_bin_name = os.path.basename(local_bin_path)
 
-    cmd = f"adb -s {ADB_ADDR} push {local_bin_path} /data/local/tmp/{remote_bin_name}"
+    cmd = f"{ADB} -s {ADB_ADDR} push {local_bin_path} /data/local/tmp/{remote_bin_name}"
     assert 0 == os.system(cmd)
 
-    cmd = f"adb -s {ADB_ADDR} shell chmod +x /data/local/tmp/{remote_bin_name}"
+    cmd = f"{ADB} -s {ADB_ADDR} shell chmod +x /data/local/tmp/{remote_bin_name}"
     assert 0 == os.system(cmd)
 
 
-def setup_gdb_server(remote_bin_name: str, args):
+def setup_gdb_server(remote_bin_name: str, ld_library_path: Optional[str], args):
     p = subprocess.Popen(
-        ["adb", "-s", ADB_ADDR, "shell"],
+        [ADB, "-s", ADB_ADDR, "shell"],
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE
     )
-    gdbserver_setup_cmd = "gdbserver64 :{} {} {}".format(
+    gdbserver_setup_cmd = "{} {} :{} {} {}".format(
+        f"LD_LIBRARY_PATH={ld_library_path}" if ld_library_path else "",
+        GDB_SERVER,
         "5039",
         "/data/local/tmp/{}".format(remote_bin_name),
         " ".join(args + [
@@ -39,4 +44,4 @@ def setup_gdb_server(remote_bin_name: str, args):
 if __name__ == "__main__":
     copy("[todo] local program path", "[todo] remote program basename")
 
-    setup_gdb_server("[todo] remote program basename", sys.argv[1:])
+    setup_gdb_server("[todo] remote program basename", None, sys.argv[1:])
